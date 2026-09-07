24762ee8235c48e50c2ca78e02fbb522e091ea2b
MOHAMMED ALHAMRASHDI <Malhamrashdi800@gmail.com>
2026-09-07 11:57:16 +0400

    DomainRegistrationRepository

--- src/main/java/com/example/demo/repositories/DomainRegistrationRepository.java
+++ src/main/java/com/example/demo/repositories/DomainRegistrationRepository.java
@@ -1,4 +1,10 @@
 package com.example.demo.repositories;
 
-public class DomainRegistrationRepository {
+import com.example.demo.entities.DomainRegistration;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DomainRegistrationRepository
+        extends JpaRepository<DomainRegistration, Long> {
 }
