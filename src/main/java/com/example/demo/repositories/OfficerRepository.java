45ef2579a017edeecb27c6ede1c57fcf8117976a
MOHAMMED ALHAMRASHDI <Malhamrashdi800@gmail.com>
2026-09-07 11:53:19 +0400

    OfficerRepository

--- src/main/java/com/example/demo/repositories/OfficerRepository.java
+++ src/main/java/com/example/demo/repositories/OfficerRepository.java
@@ -1,4 +1,10 @@
 package com.example.demo.repositories;
 
-public class OfficerRepository {
+import com.example.demo.entities.Officer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OfficerRepository
+        extends JpaRepository<Officer, Long> {
 }
