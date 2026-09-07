2b9fc6ec1480fb135657a200d43e9b048326416c
MOHAMMED ALHAMRASHDI <Malhamrashdi800@gmail.com>
2026-09-07 11:59:57 +0400

    VendorRepository

--- src/main/java/com/example/demo/repositories/VendorRepository.java
+++ src/main/java/com/example/demo/repositories/VendorRepository.java
@@ -1,4 +1,10 @@
 package com.example.demo.repositories;
 
-public class VendorRepository {
+import com.example.demo.entities.Vendor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface VendorRepository
+        extends JpaRepository<Vendor, Long> {
 }
