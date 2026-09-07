64f74f51dfbc4910c46d1470c35db51467042037
MOHAMMED ALHAMRASHDI <Malhamrashdi800@gmail.com>
2026-09-07 11:34:54 +0400

    SpectrumLicenseController

--- src/main/java/com/example/demo/controllers/SpectrumLicenseController.java
+++ src/main/java/com/example/demo/controllers/SpectrumLicenseController.java
@@ -1,4 +1,60 @@
 package com.example.demo.controllers;
 
+import com.example.demo.entities.SpectrumLicense;
+import com.example.demo.services.SpectrumLicenseService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/spectrumLicense")
 public class SpectrumLicenseController {
+
+    private final SpectrumLicenseService spectrumLicenseService;
+
+    public SpectrumLicenseController(
+            SpectrumLicenseService spectrumLicenseService) {
+
+        this.spectrumLicenseService =
+                spectrumLicenseService;
+    }
+
+    @PostMapping("/add")
+    public SpectrumLicense add(
+            @RequestBody SpectrumLicense spectrumLicense) {
+
+        return spectrumLicenseService.add(
+                spectrumLicense);
+    }
+
+    @GetMapping("/getAll")
+    public List<SpectrumLicense> getAll() {
+
+        return spectrumLicenseService.getAll();
+    }
+
+    @GetMapping("/getById/{id}")
+    public SpectrumLicense getById(
+            @PathVariable Long id) {
+
+        return spectrumLicenseService.getById(id);
+    }
+
+    @PutMapping("/update/{id}")
+    public SpectrumLicense update(
+            @PathVariable Long id,
+            @RequestBody SpectrumLicense spectrumLicense) {
+
+        return spectrumLicenseService.update(
+                id,
+                spectrumLicense);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public String delete(@PathVariable Long id) {
+
+        spectrumLicenseService.delete(id);
+
+        return "Spectrum License deleted successfully";
+    }
 }
