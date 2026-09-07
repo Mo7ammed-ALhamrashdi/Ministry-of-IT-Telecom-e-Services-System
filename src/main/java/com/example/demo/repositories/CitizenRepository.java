5d5515fe7dd8e5c377cf33bc06f8d618e71f4f2a
MOHAMMED ALHAMRASHDI <Malhamrashdi800@gmail.com>
2026-09-07 11:53:53 +0400

    CitizenRepository

--- src/main/java/com/example/demo/repositories/CitizenRepository.java
+++ src/main/java/com/example/demo/repositories/CitizenRepository.java
@@ -1,4 +1,10 @@
 package com.example.demo.repositories;
 
-public class CitizenRepository {
+import com.example.demo.entities.Citizen;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CitizenRepository
+        extends JpaRepository<Citizen, Long> {
 }
