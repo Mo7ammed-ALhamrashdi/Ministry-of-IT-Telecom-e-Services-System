a78975c92ba9095a3739ffc626237cd631e26113
MOHAMMED ALHAMRASHDI <Malhamrashdi800@gmail.com>
2026-09-07 11:32:21 +0400

    OperatorController

--- src/main/java/com/example/demo/controllers/OperatorController.java
+++ src/main/java/com/example/demo/controllers/OperatorController.java
@@ -1,4 +1,54 @@
 package com.example.demo.controllers;
 
+import com.example.demo.entities.Operator;
+import com.example.demo.services.OperatorService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/operator")
 public class OperatorController {
+
+    private final OperatorService operatorService;
+
+    public OperatorController(
+            OperatorService operatorService) {
+
+        this.operatorService = operatorService;
+    }
+
+    @PostMapping("/add")
+    public Operator add(@RequestBody Operator operator) {
+
+        return operatorService.add(operator);
+    }
+
+    @GetMapping("/getAll")
+    public List<Operator> getAll() {
+
+        return operatorService.getAll();
+    }
+
+    @GetMapping("/getById/{id}")
+    public Operator getById(@PathVariable Long id) {
+
+        return operatorService.getById(id);
+    }
+
+    @PutMapping("/update/{id}")
+    public Operator update(
+            @PathVariable Long id,
+            @RequestBody Operator operator) {
+
+        return operatorService.update(id, operator);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public String delete(@PathVariable Long id) {
+
+        operatorService.delete(id);
+
+        return "Operator deleted successfully";
+    }
 }
