f70789b080a68b970e68203b34f8796704acffa5
MOHAMMED ALHAMRASHDI <Malhamrashdi800@gmail.com>
2026-09-07 11:56:20 +0400

    PaymentRepository

--- src/main/java/com/example/demo/repositories/PaymentRepository.java
+++ src/main/java/com/example/demo/repositories/PaymentRepository.java
@@ -1,4 +1,10 @@
 package com.example.demo.repositories;
 
-public class PaymentRepository {
+import com.example.demo.entities.Payment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PaymentRepository
+        extends JpaRepository<Payment, Long> {
 }
