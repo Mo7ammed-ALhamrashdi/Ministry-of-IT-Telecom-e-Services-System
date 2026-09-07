8efa21d1b58375e58c4a8de6a4282959971277d9
MOHAMMED ALHAMRASHDI <Malhamrashdi800@gmail.com>
2026-09-07 11:37:26 +0400

    InspectionController

--- src/main/java/com/example/demo/controllers/InspectionController.java
+++ src/main/java/com/example/demo/controllers/InspectionController.java
@@ -1,4 +1,56 @@
 package com.example.demo.controllers;
 
+import com.example.demo.entities.Inspection;
+import com.example.demo.services.InspectionService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/inspection")
 public class InspectionController {
+
+    private final InspectionService inspectionService;
+
+    public InspectionController(
+            InspectionService inspectionService) {
+
+        this.inspectionService = inspectionService;
+    }
+
+    @PostMapping("/add")
+    public Inspection add(
+            @RequestBody Inspection inspection) {
+
+        return inspectionService.add(inspection);
+    }
+
+    @GetMapping("/getAll")
+    public List<Inspection> getAll() {
+
+        return inspectionService.getAll();
+    }
+
+    @GetMapping("/getById/{id}")
+    public Inspection getById(
+            @PathVariable Long id) {
+
+        return inspectionService.getById(id);
+    }
+
+    @PutMapping("/update/{id}")
+    public Inspection update(
+            @PathVariable Long id,
+            @RequestBody Inspection inspection) {
+
+        return inspectionService.update(id, inspection);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public String delete(@PathVariable Long id) {
+
+        inspectionService.delete(id);
+
+        return "Inspection deleted successfully";
+    }
 }
