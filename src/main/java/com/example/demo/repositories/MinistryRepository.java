da6a7473ecce7a67a76de8bf5b7f4fe19bda2974
MOHAMMED ALHAMRASHDI <Malhamrashdi800@gmail.com>
2026-09-07 11:52:20 +0400

    MinistryRepository

--- src/main/java/com/example/demo/repositories/MinistryRepository.java
+++ src/main/java/com/example/demo/repositories/MinistryRepository.java
@@ -1,4 +1,10 @@
 package com.example.demo.repositories;
 
-public class MinistryRepository {
+import com.example.demo.entities.Ministry;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MinistryRepository
+        extends JpaRepository<Ministry, Long> {
 }
