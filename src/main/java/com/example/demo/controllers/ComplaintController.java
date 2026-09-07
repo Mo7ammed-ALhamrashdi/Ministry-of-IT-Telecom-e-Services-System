ba02205cf2d76ecd2e64f570416e05f4f1538950
MOHAMMED ALHAMRASHDI <Malhamrashdi800@gmail.com>
2026-09-07 11:36:41 +0400

    ComplaintController

--- src/main/java/com/example/demo/controllers/ComplaintController.java
+++ src/main/java/com/example/demo/controllers/ComplaintController.java
@@ -1,4 +1,56 @@
 package com.example.demo.controllers;
 
+import com.example.demo.entities.Complaint;
+import com.example.demo.services.ComplaintService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/complaint")
 public class ComplaintController {
+
+    private final ComplaintService complaintService;
+
+    public ComplaintController(
+            ComplaintService complaintService) {
+
+        this.complaintService = complaintService;
+    }
+
+    @PostMapping("/add")
+    public Complaint add(
+            @RequestBody Complaint complaint) {
+
+        return complaintService.add(complaint);
+    }
+
+    @GetMapping("/getAll")
+    public List<Complaint> getAll() {
+
+        return complaintService.getAll();
+    }
+
+    @GetMapping("/getById/{id}")
+    public Complaint getById(
+            @PathVariable Long id) {
+
+        return complaintService.getById(id);
+    }
+
+    @PutMapping("/update/{id}")
+    public Complaint update(
+            @PathVariable Long id,
+            @RequestBody Complaint complaint) {
+
+        return complaintService.update(id, complaint);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public String delete(@PathVariable Long id) {
+
+        complaintService.delete(id);
+
+        return "Complaint deleted successfully";
+    }
 }
