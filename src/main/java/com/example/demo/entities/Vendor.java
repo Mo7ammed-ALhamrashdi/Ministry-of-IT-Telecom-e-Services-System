09be6c7e3503c81af2d72b0597bedab95806faf3
MOHAMMED ALHAMRASHDI <Malhamrashdi800@gmail.com>
2026-09-07 11:09:20 +0400

    Vendor

--- src/main/java/com/example/demo/entities/Vendor.java
+++ src/main/java/com/example/demo/entities/Vendor.java
@@ -1,4 +1,34 @@
 package com.example.demo.entities;
 
-public class Vendor {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "vendors")
+public class Vendor extends BaseClass {
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @Column(length = 150)
+    private String contactEmail;
+
+    @Column(length = 20)
+    private String phoneNumber;
+
+    @Column(length = 100)
+    private String country;
+
+    @ManyToMany(mappedBy = "vendors")
+    private List<Project> projects = new ArrayList<>();
 }
