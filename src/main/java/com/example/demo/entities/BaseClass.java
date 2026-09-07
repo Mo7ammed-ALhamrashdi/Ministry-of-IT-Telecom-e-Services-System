58138709b095eea7aa2ac1a1a19163f3319cd8e0
MOHAMMED ALHAMRASHDI <Malhamrashdi800@gmail.com>
2026-09-07 10:25:09 +0400

    BaseClass,Ministry

---
+++ src/main/java/com/example/demo/entities/BaseClass.java
@@ -0,0 +1,23 @@
+package com.example.demo.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@MappedSuperclass
+public abstract class BaseClass {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Boolean isActive;
+
+    private LocalDateTime createdDate;
+
+    private LocalDateTime updatedDate;
+}
---
+++ src/main/java/com/example/demo/entities/Ministry.java
@@ -0,0 +1,21 @@
+package com.example.demo.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "ministries")
+public class Ministry extends BaseClass {
+
+    // name
+
+    // address
+
+    // relationship with Department
+
+    // relationship with Project
+}
