1a62277e0429353909d001d4f4c8e522df4264bb
MOHAMMED ALHAMRASHDI <Malhamrashdi800@gmail.com>
2026-09-07 11:10:00 +0400

    Document

--- src/main/java/com/example/demo/entities/Document.java
+++ src/main/java/com/example/demo/entities/Document.java
@@ -1,4 +1,36 @@
 package com.example.demo.entities;
 
-public class Document {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "documents")
+public class Document extends BaseClass {
+
+    @Column(nullable = false, length = 150)
+    private String title;
+
+    @Column(nullable = false, length = 50)
+    private String type;
+
+    @Column(nullable = false)
+    private LocalDate uploadDate;
+
+    @ManyToOne
+    @JoinColumn(name = "application_id")
+    private Application application;
+
+    @ManyToOne
+    @JoinColumn(name = "project_id")
+    private Project project;
 }
