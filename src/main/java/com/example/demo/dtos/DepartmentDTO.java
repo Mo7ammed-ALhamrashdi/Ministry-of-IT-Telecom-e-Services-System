f3ec67b0aaf14149aedbf7302b12c7a18fa8b378
MOHAMMED ALHAMRASHDI <Malhamrashdi800@gmail.com>
2026-09-07 11:43:01 +0400

    DepartmentDTO

--- src/main/java/com/example/demo/dtos/DepartmentDTO.java
+++ src/main/java/com/example/demo/dtos/DepartmentDTO.java
@@ -1,4 +1,44 @@
 package com.example.demo.dtos;
 
+import com.example.demo.entities.Department;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class DepartmentDTO {
+
+    private Long id;
+    private String name;
+    private String description;
+    private Long ministryId;
+
+    public static DepartmentDTO convertToDTO(
+            Department department) {
+
+        return DepartmentDTO.builder()
+                .id(department.getId())
+                .name(department.getName())
+                .description(department.getDescription())
+                .ministryId(
+                        department.getMinistry() != null
+                                ? department.getMinistry().getId()
+                                : null
+                )
+                .build();
+    }
+
+    public static List<DepartmentDTO> convertToDTO(
+            List<Department> departments) {
+
+        return departments.stream()
+                .map(DepartmentDTO::convertToDTO)
+                .toList();
+    }
 }
