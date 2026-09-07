3c019aa9b7cfeb5940dda59e895c3f8e36673525
MOHAMMED ALHAMRASHDI <Malhamrashdi800@gmail.com>
2026-09-07 11:39:37 +0400

    VendorController

--- src/main/java/com/example/demo/controllers/VendorController.java
+++ src/main/java/com/example/demo/controllers/VendorController.java
@@ -1,4 +1,54 @@
 package com.example.demo.controllers;
 
+import com.example.demo.entities.Vendor;
+import com.example.demo.services.VendorService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/vendor")
 public class VendorController {
+
+    private final VendorService vendorService;
+
+    public VendorController(
+            VendorService vendorService) {
+
+        this.vendorService = vendorService;
+    }
+
+    @PostMapping("/add")
+    public Vendor add(@RequestBody Vendor vendor) {
+
+        return vendorService.add(vendor);
+    }
+
+    @GetMapping("/getAll")
+    public List<Vendor> getAll() {
+
+        return vendorService.getAll();
+    }
+
+    @GetMapping("/getById/{id}")
+    public Vendor getById(@PathVariable Long id) {
+
+        return vendorService.getById(id);
+    }
+
+    @PutMapping("/update/{id}")
+    public Vendor update(
+            @PathVariable Long id,
+            @RequestBody Vendor vendor) {
+
+        return vendorService.update(id, vendor);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public String delete(@PathVariable Long id) {
+
+        vendorService.delete(id);
+
+        return "Vendor deleted successfully";
+    }
 }
