b337a058b678acab4297c2e3b7c34fe3a0309e2e
MOHAMMED ALHAMRASHDI <Malhamrashdi800@gmail.com>
2026-09-07 10:43:29 +0400

    MinistryService

--- src/main/java/com/example/demo/entities/MinistryService.java
+++ src/main/java/com/example/demo/entities/MinistryService.java
@@ -1,4 +1,41 @@
 package com.example.demo.entities;
 
-public class MinistryService {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "services")
+public class MinistryService extends BaseClass {
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @Column(length = 500)
+    private String description;
+
+    @Column(nullable = false)
+    private BigDecimal fee;
+
+    @Column(nullable = false)
+    private Integer processingDays;
+
+    @ManyToOne
+    @JoinColumn(name = "department_id")
+    private Department department;
+
+    @OneToMany(mappedBy = "service")
+    private List<Application> applications = new ArrayList<>();
 }
