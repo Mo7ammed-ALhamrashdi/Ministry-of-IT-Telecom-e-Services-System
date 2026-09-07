bf0d87a8a930e4eef3ef460b26d392bf933ba40e
MOHAMMED ALHAMRASHDI <Malhamrashdi800@gmail.com>
2026-09-07 12:00:24 +0400

    DocumentRepository

--- src/main/java/com/example/demo/repositories/DocumentRepository.java
+++ src/main/java/com/example/demo/repositories/DocumentRepository.java
@@ -1,4 +1,10 @@
 package com.example.demo.repositories;
 
-public class DocumentRepository {
+import com.example.demo.entities.Document;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DocumentRepository
+        extends JpaRepository<Document, Long> {
 }
