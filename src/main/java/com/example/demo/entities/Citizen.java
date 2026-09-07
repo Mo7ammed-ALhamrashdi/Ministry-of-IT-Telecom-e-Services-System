b5cf9ef33d9ab461d35cd37f88f7b090c436ed13
MOHAMMED ALHAMRASHDI <Malhamrashdi800@gmail.com>
2026-09-07 10:40:23 +0400

    Citizen

--- src/main/java/com/example/demo/entities/Citizen.java
+++ src/main/java/com/example/demo/entities/Citizen.java
@@ -1,4 +1,40 @@
 package com.example.demo.entities;
 
-public class Citizen {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "citizens")
+public class Citizen extends BaseClass {
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @Column(nullable = false, unique = true, length = 30)
+    private String nationalId;
+
+    @Column(length = 20)
+    private String phoneNumber;
+
+    @Column(length = 150)
+    private String email;
+
+    @OneToMany(mappedBy = "citizen")
+    private List<Application> applications = new ArrayList<>();
+
+    @OneToMany(mappedBy = "citizen")
+    private List<Complaint> complaints = new ArrayList<>();
+
+    @OneToMany(mappedBy = "citizen")
+    private List<DomainRegistration> domainRegistrations = new ArrayList<>();
 }
