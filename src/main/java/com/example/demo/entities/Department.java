a7ff6bfc5f74c3646a5ae235988ac0dad429b43f
MOHAMMED ALHAMRASHDI <Malhamrashdi800@gmail.com>
2026-09-07 10:38:26 +0400

    Officer

--- src/main/java/com/example/demo/entities/Department.java
+++ src/main/java/com/example/demo/entities/Department.java
@@ -4,9 +4,14 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Setter
@@ -24,4 +29,9 @@ public class Department extends BaseClass {
     @JoinColumn(name = "ministry_id")
     private Ministry ministry;
 
+    @OneToMany(mappedBy = "department")
+    private List<Officer> officers = new ArrayList<>();
+
+    @OneToMany(mappedBy = "department")
+    private List<MinistryService> services = new ArrayList<>();
 }
--- src/main/java/com/example/demo/entities/Ministry.java
+++ src/main/java/com/example/demo/entities/Ministry.java
@@ -2,25 +2,30 @@ package com.example.demo.entities;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.List;
-import lombok.Getter;
-import lombok.Setter;
 
 @Getter
 @Setter
 @Entity
 @Table(name = "ministries")
 public class Ministry extends BaseClass {
+
     @Column(nullable = false, length = 100)
     private String name;
 
     @Column(length = 200)
     private String address;
+
     @OneToMany(mappedBy = "ministry")
     private List<Department> departments = new ArrayList<>();
 
+    @OneToMany(mappedBy = "ministry")
+    private List<Project> projects = new ArrayList<>();
 }
