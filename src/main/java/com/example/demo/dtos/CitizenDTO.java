b23f20513d7b5d8300d263a1e8768bbfccea6169
MOHAMMED ALHAMRASHDI <Malhamrashdi800@gmail.com>
2026-09-07 11:44:14 +0400

    CitizenDTO

--- src/main/java/com/example/demo/dtos/CitizenDTO.java
+++ src/main/java/com/example/demo/dtos/CitizenDTO.java
@@ -1,4 +1,42 @@
 package com.example.demo.dtos;
 
+import com.example.demo.entities.Citizen;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class CitizenDTO {
+
+    private Long id;
+    private String name;
+    private String nationalId;
+    private String phoneNumber;
+    private String email;
+
+    public static CitizenDTO convertToDTO(
+            Citizen citizen) {
+
+        return CitizenDTO.builder()
+                .id(citizen.getId())
+                .name(citizen.getName())
+                .nationalId(citizen.getNationalId())
+                .phoneNumber(citizen.getPhoneNumber())
+                .email(citizen.getEmail())
+                .build();
+    }
+
+    public static List<CitizenDTO> convertToDTO(
+            List<Citizen> citizens) {
+
+        return citizens.stream()
+                .map(CitizenDTO::convertToDTO)
+                .toList();
+    }
 }
