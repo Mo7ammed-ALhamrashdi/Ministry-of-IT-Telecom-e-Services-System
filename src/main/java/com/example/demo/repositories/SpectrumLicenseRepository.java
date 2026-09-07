285f2aabd897df914c15f6698935de0b37f00a65
MOHAMMED ALHAMRASHDI <Malhamrashdi800@gmail.com>
2026-09-07 11:56:45 +0400

    SpectrumLicenseRepository

--- src/main/java/com/example/demo/repositories/SpectrumLicenseRepository.java
+++ src/main/java/com/example/demo/repositories/SpectrumLicenseRepository.java
@@ -1,4 +1,10 @@
 package com.example.demo.repositories;
 
-public class SpectrumLicenseRepository {
+import com.example.demo.entities.SpectrumLicense;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SpectrumLicenseRepository
+        extends JpaRepository<SpectrumLicense, Long> {
 }
