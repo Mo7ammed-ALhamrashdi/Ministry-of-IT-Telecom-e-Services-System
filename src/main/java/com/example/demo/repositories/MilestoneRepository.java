2ddbc10763494a639cef0114f2f5b978a5dbf08b
MOHAMMED ALHAMRASHDI <Malhamrashdi800@gmail.com>
2026-09-07 11:59:30 +0400

    MilestoneRepository

--- src/main/java/com/example/demo/repositories/MilestoneRepository.java
+++ src/main/java/com/example/demo/repositories/MilestoneRepository.java
@@ -1,4 +1,10 @@
 package com.example.demo.repositories;
 
-public class MilestoneRepository {
+import com.example.demo.entities.Milestone;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MilestoneRepository
+        extends JpaRepository<Milestone, Long> {
 }
