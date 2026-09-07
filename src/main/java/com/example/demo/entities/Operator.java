98193062df7bf6df414725c9d25f9e083d79beaf
MOHAMMED ALHAMRASHDI <Malhamrashdi800@gmail.com>
2026-09-07 10:41:27 +0400

    Operator

--- src/main/java/com/example/demo/entities/Operator.java
+++ src/main/java/com/example/demo/entities/Operator.java
@@ -1,4 +1,40 @@
 package com.example.demo.entities;
 
-public class Operator {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "operators")
+public class Operator extends BaseClass {
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String licenseNumber;
+
+    @Column(length = 150)
+    private String contactEmail;
+
+    @Column(length = 100)
+    private String country;
+
+    @OneToMany(mappedBy = "operator")
+    private List<SpectrumLicense> spectrumLicenses = new ArrayList<>();
+
+    @OneToMany(mappedBy = "operator")
+    private List<Inspection> inspections = new ArrayList<>();
+
+    @OneToMany(mappedBy = "operator")
+    private List<Complaint> complaints = new ArrayList<>();
 }
