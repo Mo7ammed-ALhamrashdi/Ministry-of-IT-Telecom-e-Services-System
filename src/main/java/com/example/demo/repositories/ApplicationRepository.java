22b4b9968125d4d913e49bda18a0b3b7b63a9f81
MOHAMMED ALHAMRASHDI <Malhamrashdi800@gmail.com>
2026-09-07 11:55:48 +0400

    ApplicationRepository

--- src/main/java/com/example/demo/repositories/ApplicationRepository.java
+++ src/main/java/com/example/demo/repositories/ApplicationRepository.java
@@ -1,4 +1,10 @@
 package com.example.demo.repositories;
 
-public class ApplicationRepository {
+import com.example.demo.entities.Application;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ApplicationRepository
+        extends JpaRepository<Application, Long> {
 }
